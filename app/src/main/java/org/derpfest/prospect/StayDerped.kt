8ea1b365337843ae362d5f68dd46412db6c80f05
NurKeinNeid <mralexman3000@gmail.com>
2023-03-05 03:59:13 +0800

    Prospect: Add `StayDerped` widget
    Change-Id: Ibd78705136d4b63cb0317b0826bf24c2d027a56c
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

---
+++ app/src/main/java/org/derpfest/prospect/StayDerped.kt
@@ -0,0 +1,24 @@
+package org.derpfest.prospect
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class StayDerped : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+    }
+
+    override fun onEnabled(context: Context) {
+    }
+
+    override fun onDisabled(context: Context) {
+    }
+}
