ccbf2343f170a3e04f439989abed82b88d16304c
Alexander Brunswig <mralexman3000@gmail.com>
2025-01-20 17:55:12 +0100

    StayDerped: Add a way to trigger Smartspacer updates
    Change-Id: I9a7192c5370a26b26372ebc7672f2c578c76e550
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- app/src/main/java/org/derpfest/prospect/StayDerped.kt
+++ app/src/main/java/org/derpfest/prospect/StayDerped.kt
@@ -4,21 +4,31 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import com.kieronquinn.app.smartspacer.sdk.SmartspacerData
 
 /**
  * Implementation of App Widget functionality.
  */
 class StayDerped : AppWidgetProvider() {
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
-    ) {
+    
+    private fun requestSmartspacerUpdate(context: Context) {
+        // Request an update from Smartspacer
+        context.sendBroadcast(Intent(SmartspacerData.ACTION_REQUEST_UPDATE).apply {
+            `package` = "com.kieronquinn.app.smartspacer" // Package name of Smartspacer
+        })
     }
 
     override fun onEnabled(context: Context) {
+        // Called when the first widget instance is added
+        requestSmartspacerUpdate(context)
+    }
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // Update the widget
+        requestSmartspacerUpdate(context)
     }
 
     override fun onDisabled(context: Context) {
+        // Called when the last widget instance is removed
     }
 }
